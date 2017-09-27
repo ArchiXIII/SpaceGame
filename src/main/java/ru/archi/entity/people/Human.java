88db2ddcf203793ba1f9f871543e3ec44e680fce
ArchiXIII <archi.xiii@mail.ru>
2017-09-27 16:45:11 +0400

    Created classes Warship and Human.

---
+++ src/main/java/ru/archi/entity/people/Human.java
@@ -0,0 +1,23 @@
+package main.java.ru.archi.entity.people;
+
+import main.java.ru.archi.entity.Sentient;
+
+/**
+ * Created by Черный on 27.09.2017.
+ */
+public class Human implements Sentient {
+    @Override
+    public void speak() {
+
+    }
+
+    @Override
+    public void fight() {
+
+    }
+
+    @Override
+    public void shoot() {
+
+    }
+}
---
+++ src/main/java/ru/archi/entity/ships/Warship.java
@@ -0,0 +1,27 @@
+package main.java.ru.archi.entity.ships;
+
+import main.java.ru.archi.entity.Ship;
+
+/**
+ * Created by Черный on 27.09.2017.
+ */
+public class Warship extends Ship {
+    public Warship(Integer speed) {
+        super(speed);
+    }
+
+    @Override
+    public void takeoff() {
+
+    }
+
+    @Override
+    public void landing() {
+
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
