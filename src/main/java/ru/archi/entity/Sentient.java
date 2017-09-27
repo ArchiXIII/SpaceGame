9d6e1927c90d29a2677397b36f7f0ba3874a86e3
ArchiXIII <archi.xiii@mail.ru>
2017-09-27 14:02:19 +0400

    Initial commit

---
+++ src/main/java/ru/archi/entity/Sentient.java
@@ -0,0 +1,7 @@
+package main.java.ru.archi.entity;
+
+/**
+ * Created by Черный on 27.09.2017.
+ */
+public interface Sentient {
+}
---
+++ src/main/java/ru/archi/entity/Ship.java
@@ -0,0 +1,42 @@
+package main.java.ru.archi.entity;
+
+/**
+ * Created by Черный on 27.09.2017.
+ */
+public abstract class Ship {
+    int hitPoint = 100;
+    boolean isLife = true;
+    Integer speed;
+    Sentient pilot;
+
+    public Ship(Integer speed){
+        this.speed = speed;
+    }
+
+    public void setPilot(Sentient sentient){
+        pilot = sentient;
+    }
+
+    public Sentient getPilot(){
+        return pilot;
+    }
+
+    public abstract void takeoff();
+
+    public abstract void landing();
+
+    public abstract void move();
+
+    public void damage(){
+        hitPoint -= 10;
+        if(hitPoint < 1) destruction();
+    }
+
+    private void destruction() {
+        isLife = false;
+    }
+
+    public boolean getIsLife(){
+        return isLife;
+    }
+}
